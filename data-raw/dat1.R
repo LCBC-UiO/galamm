8f38d2ae0c994b29ec74f83a7b6132dde3c92ba6
Øystein Sørensen <oystein_sorensen@hotmail.com>
2021-11-05 09:16:58 +0100

    Revert "Added example data to use with linear mixed model"

--- data-raw/dat1.R
+++ data-raw/dat1.R
@@ -1,15 +1,21 @@
-## test dataset, simple linear mixed model
+## code to prepare `dat1` dataset goes here
 library(tidyverse)
-set.seed(9987)
+library(mgcv)
+# Three items measuring a single underlying trait
 n <- 100
-dat1 <- tibble(
-  id = seq_len(n),
-  tp = list(1:3),
-  b = rnorm(n)
-) %>%
-  unnest(cols = tp) %>%
-  mutate(y = .1 * tp + b + rnorm(nrow(.))) %>%
-  select(-b) %>%
+lambda <- c(1, 2, .4)
+set.seed(9987)
+dat1 <- gamSim(n = n, verbose = FALSE) %>%
+  select(x2, y) %>%
+  rename(x = x2, eta = y) %>%
+  mutate(
+    id = row_number(),
+    item1 = lambda[[1]] * eta + rnorm(n, sd = .3),
+    item2 = lambda[[2]] * eta + rnorm(n, sd = .3),
+    item3 = lambda[[3]] * eta + rnorm(n, sd = .3)
+  ) %>%
+  select(-eta) %>%
+  pivot_longer(cols = c(item1, item2, item3), names_to = "item") %>%
   as.data.frame()
 
 usethis::use_data(dat1, overwrite = TRUE)
