e87abb4ad261febfd72f57d2c88bcf6bef7673ed
Øystein Sørensen <oystein_sorensen@hotmail.com>
2021-11-05 09:20:37 +0100

    Revert "Added example data to use with linear mixed model"

--- R/data.R
+++ R/data.R
@@ -1,14 +1,5 @@
-#' Simulated dataset with continuous response and three observations per subject.
+#' Example dataset with three measurements of a single latent variable.
 #'
-#' This dataset can be used to validate the code, by comparing the estimates to
-#' those obtained from other mixed model software.
-#'
-#' \code{dat1} contains the following variables:
-#'
-#' \describe{
-#'   \item{\code{id}}{Subject identifier.}
-#'   \item{\code{tp}}{Within-subject timepoint.}
-#'   \item{\code{y}}{Response variable.}
-#' }
-#'
+#' The underlying smooth curve for the latent trait has been generated
+#' \code{mgcv::gamSim}.
 "dat1"
