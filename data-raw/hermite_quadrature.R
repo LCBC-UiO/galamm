1bfa49261df87974eff143300ab6a810c789a1b3
Øystein Sørensen <oystein_sorensen@hotmail.com>
2021-11-29 09:06:26 +0100

    save quadrature points

---
+++ data-raw/hermite_quadrature.R
@@ -0,0 +1,9 @@
+## code to prepare `hermite_quadrature` dataset goes here
+library(gaussquad)
+Quadpoints <- hermite.h.quadrature.rules(100)
+
+hermite_quadrature <- lapply(Quadpoints, function(qp){
+  sweep(qp, 2, c(sqrt(2), 1 / sqrt(pi)), "*")
+})
+
+usethis::use_data(hermite_quadrature, overwrite = TRUE)
