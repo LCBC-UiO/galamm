b4610571354f9b4e9f7dae52ffca25ecda50e2a0
Øystein Sørensen <oystein_sorensen@hotmail.com>
2022-04-07 16:20:25 +0200

    fixed some setup issues

---
+++ R/galamm-package.R
@@ -0,0 +1,12 @@
+#' galamm: Maximum Likelihood Estimation of Generalized Additive Latent and
+#' Mixed Models.
+#'
+#' @description This package performs maximum likelihood estmation of
+#' generalized additive latent and mxied models.
+#'
+#' @docType package
+#' @name galamm-package
+#'
+#' @importFrom stats gaussian glm family
+#'
+NULL
--- R/galamm.R
+++ R/galamm.R
@@ -3,8 +3,9 @@
 #' @param formula a two-sided formula object using \code{lme4} style to specify
 #' random effects.
 #' @param data data frame containing the variables in \code{formula}.
-#' @param family a GLM family, see \code{\link{stats::glm}} and
-#' \code{\link{stats::family}}.
+#' @param family a GLM family, see \code{\link{glm}} and
+#' \code{\link{family}}.
+#'
 #'
 #' @return
 #' @export
