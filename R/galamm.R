048810e243030ceca12f9ad6fcca1e33f9564e8d
Øystein Sørensen <oystein_sorensen@hotmail.com>
2022-04-07 16:13:10 +0200

    set up package structure

---
+++ R/galamm.R
@@ -0,0 +1,16 @@
+#' Fitting Generalized Additive Latent and Mixed Models
+#'
+#' @param formula a two-sided formula object using \code{lme4} style to specify
+#' random effects.
+#' @param data data frame containing the variables in \code{formula}.
+#' @param family a GLM family, see \code{\link{stats::glm}} and
+#' \code{\link{stats::family}}.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' # empty example
+galamm <- function(formula, data, family = gaussian){
+
+}
