d7a53568308f81de6169f9c4af4f96ad9a3a7dbd
Øystein Sørensen <oystein_sorensen@hotmail.com>
2022-04-08 08:32:49 +0200

    added test for load variable in data

--- R/galamm.R
+++ R/galamm.R
@@ -5,7 +5,8 @@
 #' @param data data frame containing the variables in \code{formula}.
 #' @param family a GLM family, see \code{\link{glm}} and
 #' \code{\link{family}}.
-#' @param latent Formula for latent variables.
+#' @param latent Formula for latent variables, of the form
+#' \code{~(factor | load_var)}.
 #' @param lambda Initial matrix for factor loadings.
 #'
 #' @return Object of class \code{galamm}.
@@ -17,10 +18,18 @@ galamm <- function(formula, data, family = gaussian,
                    latent = NULL, lambda = NULL){
 
   barlist <- lme4::findbars(latent)
-  load_vars <- lapply(barlist, function(x) as.character(x)[[2]])
-  if(any(load_vars %in% names(data))){
-    stop("Variables in argument 'latent' cannot be columns of 'data'.")
+  factors <- lapply(barlist, function(x) as.character(x)[[2]])
+
+  if(any(factors %in% names(data))){
+    stop("Factors in argument 'latent' cannot be columns of 'data'.")
   }
-  #lme4::mkReTrms(bars = barlist, fr = data)
+
+  load_vars <- lapply(barlist, function(x) as.character(x)[[3]])
+  if(!all(load_vars %in% names(data))){
+    stop("All loading variables in 'latent' must be columns of 'data'.")
+  }
+
+
+  lme4::mkReTrms(bars = barlist, fr = data)
 
 }
--- tests/testthat/test-galamm.R
+++ tests/testthat/test-galamm.R
@@ -1,7 +1,7 @@
 latent_response_dat <- readRDS(
   system.file("testdata", "latent_response_dat.rds", package = "galamm"))
 
-test_that("galamm fails when loading is a data column", {
+test_that("galamm fails when factor is a data column", {
   expect_error(galamm(
     formula = y ~ 1,
     data = latent_response_dat,
@@ -10,7 +10,16 @@ test_that("galamm fails when loading is a data column", {
   ))
 })
 
-#
+test_that("galamm fails when load variable is not in data", {
+  expect_error(galamm(
+    formula = y ~ 1,
+    data = latent_response_dat,
+    family = binomial,
+    latent = ~ (1 | id_unknown)
+  ))
+})
+
+
 # test_that("model matrices get correctly set up", {
 #   mod <- galamm(
 #     formula = y ~ item + time : latent + ( 0 + latent | id / tp ),
