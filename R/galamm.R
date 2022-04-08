a816844fca37f1635867fc0b1b1d4ba1d60c0ebf
Øystein Sørensen <oystein_sorensen@hotmail.com>
2022-04-08 10:03:11 +0200

    started adding mapping for fixed effect matrix

--- R/galamm.R
+++ R/galamm.R
@@ -17,19 +17,43 @@
 galamm <- function(formula, data, family = gaussian,
                    latent = NULL, lambda = NULL){
 
-  barlist <- lme4::findbars(latent)
-  factors <- lapply(barlist, function(x) as.character(x)[[2]])
+  latent_barlist <- lme4::findbars(latent)
+  factors <- lapply(latent_barlist, function(x) as.character(x)[[2]])
 
   if(any(factors %in% names(data))){
     stop("Factors in argument 'latent' cannot be columns of 'data'.")
   }
 
-  load_vars <- lapply(barlist, function(x) as.character(x)[[3]])
+  load_vars <- lapply(latent_barlist, function(x) as.character(x)[[3]])
   if(!all(load_vars %in% names(data))){
     stop("All loading variables in 'latent' must be columns of 'data'.")
   }
+  if(!all(load_vars %in% names(lambda))){
+    stop("All loading variables specified in 'latent' must be in 'lambda'.")
+  }
 
+  lambda_init <- lapply(lambda, function(x) {
+    x[is.na(x)] <- runif(sum(is.na(x)), .9, 1.1)
+    x
+  })
 
-  lme4::mkReTrms(bars = barlist, fr = data)
+  for(i in seq_along(latent_barlist)){
+    ff <- factors[[i]]
+    lv <- load_vars[[i]]
+    eval(parse(text = paste0("data$", ff, " <- lambda_init$", lv,
+                             "[data$item]")))
+  }
+
+  X <- model.matrix(lme4::nobars(formula), data = data)
+  # Which columns of X correspond to which latent variable
+  fixed_mappings <- Map(function(ff, lv){
+    col_inds <- grep(paste0("(?<![a-zA-Z])", ff, "(?![a-zA-Z])"),
+                     colnames(X), perl = TRUE)
+    list(
+      col_inds = col_inds,
+      lambda_ind = data[[lv]]
+    )
+  }, ff = factors, lv = load_vars)
+
 
 }
--- tests/testthat/test-galamm.R
+++ tests/testthat/test-galamm.R
@@ -7,7 +7,8 @@ test_that("galamm fails when factor is a data column", {
     data = latent_response_dat,
     family = binomial,
     latent = ~ (time | item)
-  ))
+  ),
+  regexp = "Factors in argument 'latent' cannot be columns of 'data'.")
 })
 
 test_that("galamm fails when load variable is not in data", {
@@ -16,16 +17,28 @@ test_that("galamm fails when load variable is not in data", {
     data = latent_response_dat,
     family = binomial,
     latent = ~ (1 | id_unknown)
-  ))
+  ),
+  regexp = "All loading variables in 'latent' must be columns of 'data'.")
+})
+
+test_that("galamm fails when load variable is not in lambda", {
+  expect_error(galamm(
+    formula = y ~ 1,
+    data = latent_response_dat,
+    family = binomial,
+    latent = ~ (1 | item),
+    lambda = list(other_item = c(1, NA, NA))
+  ),
+  regexp = "All loading variables specified in 'latent' must be in 'lambda'.")
 })
 
 
-# test_that("model matrices get correctly set up", {
-#   mod <- galamm(
-#     formula = y ~ item + time : latent + ( 0 + latent | id / tp ),
-#     data = latent_response_dat,
-#     family = binomial,
-#     latent = ~ (latent | item),
-#     lambda = c(1, NA_real_, NA_real_)
-#   )
-# })
+test_that("model matrices get correctly set up", {
+  mod <- galamm(
+    formula = y ~ item + latent + time : latent + ( 0 + latent | id / tp ),
+    data = latent_response_dat,
+    family = binomial,
+    latent = ~ (latent | item),
+    lambda = list(item = c(1, NA_real_, NA_real_))
+  )
+})
