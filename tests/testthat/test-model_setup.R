3ef28d409398a7d1ffa536fdf20fba16c54323ac
Øystein Sørensen <oystein_sorensen@hotmail.com>
2022-04-08 13:26:37 +0200

    added more unit tests

--- tests/testthat/test-model_setup.R
+++ tests/testthat/test-model_setup.R
@@ -1,3 +1,65 @@
+latent_response_dat <- readRDS(
+  system.file("testdata", "latent_response_dat.rds", package = "galamm"))
+
+
+test_that("galamm fails when factor is a data column", {
+  expect_error(galamm(
+    formula = y ~ 1,
+    data = latent_response_dat,
+    family = binomial,
+    latent = ~ (time | item)
+  ),
+  regexp = "Factors in argument 'latent' cannot be columns of 'data'.")
+})
+
+test_that("find_factors works", {
+  expect_equal(
+    find_factors(lme4::findbars(~ (lambda | item) + (kappa | totem)),
+                 data = data.frame(x = 1)),
+    list("lambda", "kappa"))
+})
+
+test_that("galamm fails when load variable is not in data", {
+  expect_error(galamm(
+    formula = y ~ 1,
+    data = latent_response_dat,
+    family = binomial,
+    latent = ~ (1 | id_unknown)
+  ),
+  regexp = "All loading variables in 'latent' must be columns of 'data'.")
+})
+
+test_that("galamm fails when load variable is not in lambda", {
+  expect_error(galamm(
+    formula = y ~ 1,
+    data = latent_response_dat,
+    family = binomial,
+    latent = ~ (1 | item),
+    lambda = list(other_item = c(1, NA, NA))
+  ),
+  regexp = "All loading variables specified in 'latent' must be in 'lambda'.")
+})
+
+test_that("find_load_vars works", {
+  expect_equal(
+    find_load_vars(lme4::findbars(~ (lambda | item) + (kappa | jtem)),
+                   data = data.frame(item = 1, jtem = 2),
+                   lambda = list(item = 1, jtem = 2)),
+    list("item", "jtem"))
+})
+
+test_that("initialize_lambda works", {
+
+  set.seed(2)
+  ret <- initialize_lambda(list(item1 = c(1, NA, NA),
+                                item2 = c(1, 1, NA, NA, NA)))
+
+  expect_equal(names(ret), c("item1", "item2"))
+  expect_equal(attr(ret$item1, "free_inds"), c(FALSE, TRUE, TRUE))
+  expect_equal(attr(ret$item2, "free_inds"), c(FALSE, FALSE, TRUE, TRUE, TRUE))
+  expect_equal(c(ret$item1[[1]], ret$item2[1:2]), c(1, 1, 1))
+})
+
 test_that("update_fixed works", {
   X <- matrix(1:12, nrow = 3)
   expect_equal(
