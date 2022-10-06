b512db83f7ec3e72f2b137f49adbc43c18ef81f4
Øystein Sørensen <oystein_sorensen@hotmail.com>
2022-10-06 12:41:13 +0200

    Added a unit test with varying number of trials for mixed response

--- tests/testthat/test-marginal-likelihood-mixed-response.R
+++ tests/testthat/test-marginal-likelihood-mixed-response.R
@@ -58,6 +58,7 @@ opt <- optim(opt$par, fn, gr, epsilon_u = 1e-5, method = "L-BFGS-B",
 
 fmod <- mlmem(opt$par, TRUE)
 
+
 test_that("mixed response works", {
   expect_equal(opt$par, c(0.95732902946743, 0.0114817102301233))
   expect_equal(opt$value, -4646.14884571025)
@@ -66,5 +67,75 @@ test_that("mixed response works", {
                                          -684.949541922932), dim = c(2L, 2L)))
 })
 
+
+# Test with varying number of trials
+set.seed(100)
+dat <- tibble(id = 1:1000) %>%
+  mutate(b = rnorm(nrow(.))) %>%
+  uncount(4, .id = "item") %>%
+  mutate(
+    trials = as.numeric(sample(10, nrow(.), replace = TRUE)),
+    y = pmap_dbl(
+      list(b, item, trials), ~ if_else(..2 %in% c(1, 2), rnorm(1, ..1), as.numeric(rbinom(1, ..3, plogis(..1)))))
+  )
+
+lmod <- lFormula(y ~ (1 | id), data = dat)
+
+theta_inds <- 1
+beta_inds <- 2
+
+mlwrapper <- function(par, hessian = FALSE, epsilon_u = .1){
+  marginal_likelihood(
+    y = dat$y,
+    trials = dat$trials,
+    X = lmod$X,
+    Zt = lmod$reTrms$Zt,
+    Lambdat = lmod$reTrms$Lambdat,
+    beta = par[beta_inds],
+    theta = par[theta_inds],
+    theta_mapping = lmod$reTrms$Lind - 1L,
+    lambda = numeric(),
+    lambda_mapping_X = integer(),
+    lambda_mapping_Zt = integer(),
+    weights = numeric(),
+    weights_mapping = integer(),
+    family = c("gaussian", "binomial"),
+    family_mapping = if_else(dat$item %in% c(1, 2), 0L, 1L),
+    maxit_conditional_modes = 10,
+    hessian = hessian,
+    epsilon_u = epsilon_u
+  )
+}
+
+mlmem <- memoise(mlwrapper)
+fn <- function(par, epsilon_u = .1){
+  mlmem(par, epsilon_u = epsilon_u)$logLik
+}
+gr <- function(par, epsilon_u = .1){
+  mlmem(par, epsilon_u = epsilon_u)$gradient
+}
+
+opt <- optim(c(1, 0), fn, gr, epsilon_u = 1, method = "L-BFGS-B",
+             lower = c(0, -Inf), control = list(fnscale = -1))
+
+opt <- optim(opt$par, fn, gr, epsilon_u = .1, method = "L-BFGS-B",
+             lower = c(0, -Inf), control = list(fnscale = -1))
+
+opt <- optim(opt$par, fn, gr, epsilon_u = .01, method = "L-BFGS-B",
+             lower = c(0, -Inf), control = list(fnscale = -1))
+
+opt <- optim(opt$par, fn, gr, epsilon_u = .001, method = "L-BFGS-B",
+             lower = c(0, -Inf), control = list(fnscale = -1))
+
+
+fmod <- mlmem(opt$par, TRUE)
+
+test_that("mixed response varying number of trials works", {
+  expect_equal(opt$par, c(0.872205160510487, 0.0166011748937604))
+  expect_equal(opt$value, -6322.91801530508)
+  expect_equal(fmod$phi, c(1.31544526738152, 1))
+  expect_equal(fmod$hessian, structure(c(-1120.14782091153, -0.122199502560083, -0.122199502560083,
+                                         -785.439816986964), dim = c(2L, 2L)))
+})
 
 
