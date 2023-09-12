ae971694619075da7f88203d64daf95ee32cd710
Øystein Sørensen <oystein_sorensen@hotmail.com>
2023-09-12 11:12:57 +0200

    Semiparametric is working. Added tests.

--- R/galamm.R
+++ R/galamm.R
@@ -25,7 +25,6 @@ galamm <- function(formula, weights = NULL, data, family = gaussian,
                    family_mapping = rep(1L, nrow(data)),
                    load.var = NULL, lambda = NULL, factor = NULL,
                    start = NULL, control = galamm_control()) {
-
   stopifnot(length(family) == length(unique(family_mapping)))
 
   data <- as.data.frame(data)
@@ -144,7 +143,7 @@ galamm <- function(formula, weights = NULL, data, family = gaussian,
           return(rep(-1L, sum(delta)))
         }
 
-        mapping_component <- rep(NA_integer_, sum(delta))
+        mapping_component <- rep(NA_integer_, length(delta))
         for (j in seq_along(cnms)) {
           cn <- unlist(lapply(factor[[f]], function(x) {
             m <- regexpr(x, cnms[[j]], fixed = TRUE)
@@ -152,18 +151,18 @@ galamm <- function(formula, weights = NULL, data, family = gaussian,
           }))
 
           inds <- which(data[, cn] != 0)
-          inds_expanded <- unlist(Map(function(x, y) rep(x, each = y), x = inds, y = delta[inds]))
-          if (any(delta[inds] > 1) && !any(delta[inds] == 0)) inds_expanded <- order(inds_expanded)
+          inds_expanded <- unlist(Map(function(x, y) rep(x, each = y), x = inds, y = pmin(1, delta[inds])))
+          # if (any(delta[inds] > 1) && !any(delta[inds] == 0)) inds_expanded <- order(inds_expanded)
           mapping_component[inds_expanded] <-
-            unlist(Map(function(x, y) rep(ll[x, cn], each = y),
+            Map(function(x, y) rep(ll[x, cn], each = y),
               x = data[inds, load.var], y = delta[inds]
-            ))
+            )
         }
 
         mapping_component
       })
 
-      lambda_mapping_Zt <- as.numeric(do.call(rbind, mappings))
+      lambda_mapping_Zt <- unlist(do.call(function(...) mapply(c, ..., SIMPLIFY = FALSE), mappings))
       lambda_mapping_Zt <- lambda_mapping_Zt[!is.na(lambda_mapping_Zt)]
 
       stopifnot(length(lambda_mapping_Zt) == sum(diff(Zt@p)))
