7f12f3bb32176802ad822042708e1500d0eb3871
osorensen <osorensen@users.noreply.github.com>
2023-09-13 13:46:02 +0000

    Style code (GHA)

--- tests/testthat/test-galamm-setup.R
+++ tests/testthat/test-galamm-setup.R
@@ -16,12 +16,18 @@ test_that("wrong input is handled properly", {
 
 test_that("family can be defined in three different ways", {
   dat <- hsced[1:100, ]
-  mod1 <- galamm(formula = y ~ x + (1 | id), data = dat, family = gaussian,
-                 control = galamm_control(optim_control = list(maxit = 1)))
-  mod2 <- galamm(formula = y ~ x + (1 | id), data = dat, family = "gaussian",
-                 control = galamm_control(optim_control = list(maxit = 1)))
-  mod3 <- galamm(formula = y ~ x + (1 | id), data = dat, family = gaussian(),
-                 control = galamm_control(optim_control = list(maxit = 1)))
+  mod1 <- galamm(
+    formula = y ~ x + (1 | id), data = dat, family = gaussian,
+    control = galamm_control(optim_control = list(maxit = 1))
+  )
+  mod2 <- galamm(
+    formula = y ~ x + (1 | id), data = dat, family = "gaussian",
+    control = galamm_control(optim_control = list(maxit = 1))
+  )
+  mod3 <- galamm(
+    formula = y ~ x + (1 | id), data = dat, family = gaussian(),
+    control = galamm_control(optim_control = list(maxit = 1))
+  )
 
   expect_equal(logLik(mod1), logLik(mod2))
   expect_equal(logLik(mod2), logLik(mod3))
