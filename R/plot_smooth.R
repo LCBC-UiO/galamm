ecccd4693add9a0397d398530970e0ad37b30e47
Øystein Sørensen <oystein_sorensen@hotmail.com>
2023-09-14 17:01:56 +0200

    Added to the vignette on semiparametric estimation

---
+++ R/plot_smooth.R
@@ -0,0 +1,28 @@
+plot_smooth <- function(object, ...){
+  UseMethod("plot_smooth")
+}
+
+#' Plot smooth terms for galamm fits
+#'
+#' Plots smooth terms of a fitted \code{galamm} object. This function is a thin
+#' wrapper around \code{mgcv::plot.gam}
+#' \insertCite{woodGeneralizedAdditiveModels2017a}{galamm}.
+#'
+#' @aliases plot_smooth plot_smooth.galamm
+#' @export plot_smooth
+#' @export
+#'
+#' @param object Object of class \code{galamm} returned from
+#'   \code{\link{galamm}}.
+#' @param ... Other optional arguments, passed on to \code{mgcv::plot.gam}.
+#'
+#' @return A plot is displayed on the screen.
+#' @export
+#'
+#' @references \insertAllCited{}
+#'
+plot_smooth.galamm <- function(object, ...) {
+  if(!exists("gam", object)) stop("No terms to plot.")
+
+  plot(object$gam, ...)
+}
