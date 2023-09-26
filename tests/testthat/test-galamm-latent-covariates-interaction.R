6019f0443600df6bc1dce920c9c347ab9f670285
Øystein Sørensen <oystein_sorensen@hotmail.com>
2023-09-26 13:48:04 +0200

    added test for smooth terms with latent-observed interactions

--- tests/testthat/test-galamm-latent-covariates-interaction.R
+++ tests/testthat/test-galamm-latent-covariates-interaction.R
@@ -70,33 +70,34 @@ test_that("Crossed latent-observed interaction models work", {
 
 })
 
-#
-# test_that("Latent-observed interaction with smooths", {
-#   data <- latent_covariates
-#   data$m1 <- as.numeric(data$type == "measurement1")
-#   data$m2 <- as.numeric(data$type == "measurement2")
-#   formula <- y ~ 0 + m1 + m2 + s(x, k = 4, by = response) +
-#     (0 + loading | id)
-#   weights <- NULL
-#
-#   family <- gaussian
-#   family_mapping <- rep(1L, nrow(data))
-#   load.var <- "type"
-#   lambda <- list(matrix(c(1, NA, NA), ncol = 1))
-#   factor <- list("loading")
-#   factor_interactions <- list(list(~1, ~1, ~x))
-#   start <- NULL
-#   control <- galamm_control()
-#
-#   mod <- galamm(
-#     formula = formula,
-#     data = data,
-#     load.var = load.var,
-#     lambda = lambda,
-#     factor = factor,
-#     factor_interactions = factor_interactions
-#   )
-#
-#   expect_equal(deviance(mod), 1840.38, tolerance = 1e-3)
-# })
-#
+
+test_that("Latent-observed interaction with smooths", {
+  data <- latent_covariates
+  data$m1 <- as.numeric(data$type == "measurement1")
+  data$m2 <- as.numeric(data$type == "measurement2")
+  formula <- y ~ 0 + m1 + m2 + s(x, k = 4, by = response) +
+    (0 + loading | id)
+
+  load.var <- "type"
+  lambda <- list(matrix(c(1, NA, NA), ncol = 1))
+  factor <- list("loading")
+  factor_interactions <- list(list(~1, ~1, ~x))
+
+  mod <- galamm(
+    formula = formula,
+    data = data,
+    load.var = load.var,
+    lambda = lambda,
+    factor = factor,
+    factor_interactions = factor_interactions
+  )
+
+  expect_equal(deviance(mod), 120.311, tolerance = 1e-3)
+  expect_equal(mod$gam$edf,
+               c(m1 = 0.999999999999793, m2 = 1.00000000000022,
+                 `s(x):response.1` = 1.76038497918713e-10,
+                 `s(x):response.2` = 2.52273958510005e-11,
+                 `s(x):response.3` = 1, `s(x):response.4` = 1),
+               tolerance = .1)
+})
+
