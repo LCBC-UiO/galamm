0853037ce0a6e1d77ae2b6096c00dc68f4bcbd13
Øystein Sørensen <oystein_sorensen@hotmail.com>
2023-10-16 14:32:01 +0200

    added tests with colinear predictors

--- tests/testthat/test-galamm-setup.R
+++ tests/testthat/test-galamm-setup.R
@@ -537,4 +537,13 @@ test_that("galamm rejects perfectly noiseless input data", {
       data = dat
     )
   }, "Deterministic relationship between response and fixed effects.")
+
+  dat <- IRTsim
+  dat$item2 <- as.factor(dat$item)
+  expect_message({
+    mod <- galamm(
+      formula = y ~ 0 + as.factor(item) + as.factor(item2) + (1 | school),
+      data = dat
+    )
+  }, "fixed-effect model matrix is rank deficient so dropping 4 columns / coefficients")
 })
