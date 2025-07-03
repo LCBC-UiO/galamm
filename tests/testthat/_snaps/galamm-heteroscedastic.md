ab25101b034a6816def5ca3e933b616263dd0cc8
Øystein Sørensen <oystein_sorensen@hotmail.com>
2025-07-03 14:54:47 +0200

    solve #193

--- tests/testthat/_snaps/galamm-heteroscedastic.md
+++ tests/testthat/_snaps/galamm-heteroscedastic.md
@@ -32,3 +32,37 @@
       
       
 
+# Heteroscedastic model works with more than one group
+
+    Code
+      print(summary(mod), digits = 3)
+    Output
+      GALAMM fit by maximum marginal likelihood.
+      Formula: y ~ x + (1 | id)
+         Data: hsced2
+      Weights: ~(1 | item)
+      
+           AIC      BIC   logLik deviance df.resid 
+        4128.3   4158.8  -2058.1   4116.3     1194 
+      
+      Scaled residuals: 
+         Min     1Q Median     3Q    Max 
+      -5.656 -0.709  0.028  0.683  4.323 
+      
+      Random effects:
+       Groups   Name        Variance Std.Dev.
+       id       (Intercept) 0.988    0.994   
+       Residual             0.960    0.980   
+      Number of obs: 1200, groups:  id, 200
+      
+      Variance function:
+         1    2    3 
+      1.00 2.01 1.98 
+      
+      Fixed effects:
+                  Estimate Std. Error t value Pr(>|t|)
+      (Intercept)    0.129     0.0992    1.30 1.94e-01
+      x              0.706     0.1213    5.82 5.88e-09
+      
+      
+
--- tests/testthat/test-galamm-heteroscedastic.R
+++ tests/testthat/test-galamm-heteroscedastic.R
@@ -59,3 +59,58 @@ test_that("Heteroscedastic model works", {
     tolerance = .01
   )
 })
+
+test_that("Heteroscedastic model works with more than one group", {
+  set.seed(123)
+  hsced2 <- hsced
+  hsced2$item[hsced2$item == 2] <- sample(c(2, 3), sum(hsced2$item == 2), replace = TRUE)
+
+  mod <- galamm(
+    formula = y ~ x + (1 | id),
+    weights = ~ (1 | item),
+    data = hsced2
+  )
+
+  expect_snapshot(print(summary(mod), digits = 3))
+  expect_equal(mod$model$loglik, -2058.13575138403)
+  expect_equal(
+    summary(mod)$AICtab,
+    c(AIC = 4128.27150276806, BIC = 4158.81196378272,
+      logLik = -2058.13575138403,
+      deviance = 4116.27150276806, df.resid = 1194)
+  )
+  expect_equal(summary(mod)$Lambda, NULL)
+
+  expect_equal(
+    residuals(mod)[c(4, 8, 11)],
+    c(0.881516323889496, -1.83825643847652, 0.77018964982384)
+  )
+
+  expect_equal(vcov(mod, parm = "weights"),
+               structure(c(0.00341412313076528, 0.00142678601570138, 0.00142678601570138,
+                           0.00357562468779565), dim = c(2L, 2L)),
+               tolerance = 1e-4
+  )
+
+  expect_equal(vcov(mod, parm = 1L),
+               structure(0.00543350591426884, dim = c(1L, 1L)),
+               tolerance = 1e-4
+  )
+
+  expect_error(vcov(mod, parm = 6L), "out of bounds")
+  expect_error(vcov(mod, parm = "phi"), "Parameter not found")
+
+  # Now use initial values
+  mod_start <- galamm(
+    formula = y ~ x + (1 | id),
+    weights = ~ (1 | item),
+    data = hsced2,
+    start = list(beta = c(.13, .70), theta = 1.01, weights = c(.501, .44))
+  )
+
+  expect_equal(
+    mod_start$parameters$parameter_estimates,
+    mod$parameters$parameter_estimates,
+    tolerance = .01
+  )
+})
